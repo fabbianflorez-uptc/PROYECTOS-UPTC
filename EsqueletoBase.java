dafdd38ee6572cac4f74a7fc5832ecb011503e18
FABIAN L. FLOREZ G. <fabian.florez03@uptc.edu.co>
2026-04-07 12:35:26 -0500

    Update EsqueletoBase.java
    Se añadieron mas anotaciones para comprender mejor la estructura base.

--- EsqueletoBase.java
+++ EsqueletoBase.java
@@ -3,8 +3,19 @@
 //---CODIGO 202614060--------------------//
 //---------------------------------:D----//
 
-public class EsqueletoBase { // El nombre debe ser igual al del archivo para que todo funcione correctamente
-    public static void main(String[] args) { // Este es el "corazon" del programa
-        // Aqui va el codigo que se ejecuta tiene que estar entre las llaves o no funcionará.
+public class MiPrimerPrograma001 { // "public class" es el nombre de tu archivo. Si el archivo se llama diferente, saca error.
+    
+    // "public static void main" es el PUNTO DE ENTRADA. 
+    // Sin esta linea, el play del programa no sirve para nada.
+    public static void main(String[] args) { 
+        
+        // "System.out.println" sirve para tirar texto a la consola.
+        // System = El sistema de la conputadora.
+        // out = Salida de datos.
+        // println = Imprimir una linea de texto (print line).
+        System.out.println("Estructura base lista para programar."); 
+        
+        // RECUERDA: Cada instruccion termina en punto y comaaaa (;) 
+        // Si se te olvida, el codigo no conpila D:.
     }
 }
