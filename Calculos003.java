88b8c6d428b5549b6f2d33cb9274f8437916efe3
FABIAN L. FLOREZ G. <fabian.florez03@uptc.edu.co>
2026-04-07 12:40:34 -0500

    Create Calculos003.java
    La mejor forma de iniciar en Java es con una calculadora. Aquí hay un  ejemplo en el cual se puede entender como usar los simbolos matematicos.

---
+++ Calculos003.java
@@ -0,0 +1,29 @@
+//---------------------------------------//
+//--FABIAN LEONARDO FLOREZ GONZALEZ------//
+//---CODIGO 202614060--------------------//
+//---------------------------------:D----//
+
+public class Calculos003 {
+    public static void main(String[] args) {
+        // Declaramos dos numeros para las pruebas
+        double num1 = 10.5;
+        double num2 = 2.0;
+        
+        // Suma (+)
+        double suma = num1 + num2;
+        
+        // Resta (-)
+        double resta = num1 - num2;
+        
+        // Multiplicasion (*)
+        double multi = num1 * num2;
+        
+        // Dibision (/)
+        double divi = num1 / num2;
+        
+        // Modulo (%) = Es el residuo que sobra de una dibision. Muy util.
+        double residuo = num1 % num2;
+        
+        System.out.println("Resultados: Suma=" + suma + " | Residuo=" + residuo);
+    }
+}
