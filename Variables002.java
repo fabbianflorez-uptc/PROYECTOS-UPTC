d23b9f8fbd14666598578a2072a2e8fc87a3e5af
FABIAN L. FLOREZ G. <fabian.florez03@uptc.edu.co>
2026-04-07 12:30:36 -0500

    Create Variables002.java
    En Java tienes que desirle a la conputadora qué tipo de dato vas a guardar. No puedes meter un nombre en una caja de números.

---
+++ Variables002.java
@@ -0,0 +1,27 @@
+//---------------------------------------//
+//--FABIAN LEONARDO FLOREZ GONZALEZ------//
+//---CODIGO 202614060--------------------//
+//---------------------------------:D----//
+
+public class Variables002 {
+    public static void main(String[] args) {
+        
+        // int = Integer (Entero). Solo numeros sin decimales.
+        int edad = 19; 
+        
+        // double = Numeros con coma o punto decimal. Se usa para estaturas o precios.
+        double estatura = 1.64; 
+        
+        // String = Cadena de caracteres (Texto). Siempre con S mayuscula y comillas dobles "".
+        String ciudad = "Soatá"; 
+        
+        // char = Character (Caracter). Solo una letra o simbolo, usa comillas simples ''.
+        char inicial = 'F'; 
+        
+        // boolean = Solo tiene dos estados: verdad (true) o mentira (false).
+        boolean esIngeniero = false; 
+
+        // El simbolo + sirve para "concatenar", osea, pegar el texto con la bariable.
+        System.out.println("Mi inicial es " + inicial + " y vivo en " + ciudad);
+    }
+}
