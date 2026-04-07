275bc0762b0dbf0e8937e79afcf49134e1a1d47c
FABIAN L. FLOREZ G. <fabian.florez03@uptc.edu.co>
2026-04-07 12:24:51 -0500

    Create EsqueletoBase.java
    Este es el esqueleto de cualquier programa en Java. Sin esta base, nada funciona. Define el archivo y el punto de entrada (main).

---
+++ EsqueletoBase.java
@@ -0,0 +1,10 @@
+//---------------------------------------//
+//--FABIAN LEONARDO FLOREZ GONZALEZ------//
+//---CODIGO 202614060--------------------//
+//---------------------------------:D----//
+
+public class EsqueletoBase { // El nombre debe ser igual al del archivo para que todo funcione correctamente
+    public static void main(String[] args) { // Este es el "corazon" del programa
+        // Aqui va el codigo que se ejecuta tiene que estar entre las llaves o no funcionará.
+    }
+}
